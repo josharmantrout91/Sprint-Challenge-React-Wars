2b5be11d1e584022dfa05c2e5ada2ddcfe01a271
josharmantrout91 <josh.armantrout@gmail.com>
2019-01-11 20:49:16 -0500

    added props to Character.js

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -4,7 +4,15 @@ import './StarWars.css';
 const Character = props => {
     return(
         <div className="character-stats">            
-                <p>{ props.starWarsChar.name } </p>
+                <p>{ props.starWarsChar.name }</p>
+                <p className="stat">Birth Year: { props.starWarsChar.birth_year }</p>
+                <p className="stat">Gender: { props.starWarsChar.gender }</p>
+                <p className="stat">Height: { props.starWarsChar.height }</p>
+                <p className="stat">Mass: { props.starWarsChar.mass }</p>
+                <p className="stat">Hair Color: { props.starWarsChar.hair_color }</p>
+                <p className="stat">Eye Color: { props.starWarsChar.eye_color }</p>
+                <p className="stat">Hair Color: { props.starWarsChar.hair_color }</p>
+                <p className="stat">Skin Color: { props.starWarsChar.skin_color }</p>
         </div>
  );
 }
